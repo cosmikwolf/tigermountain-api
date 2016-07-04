ef2794654efac8a3e222aecd96087162778ef45f
Tenkai Kariya <tenkai@zetaohm.com>
2016-07-04 13:59:42 -0700

    ordering
    Signed-off-by: Tenkai Kariya <tenkai@zetaohm.com>

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ApplicationRecord
-  has_many :holdings
+  has_many :holdings,  -> { order(:sharesheld => :desc) }
 
   def self.all_cached
     Rails.cache.fetch('Issue.all') { all }
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,6 @@
 class Owner < ApplicationRecord
-  has_many :holdings
+  has_many :holdings,  -> { order(:portfoliopercent => :desc).limit(25) }
+
   def self.all_cached
     Rails.cache.fetch('Owner.all') { all }
   end
